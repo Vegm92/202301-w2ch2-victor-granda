2c90b83649a3b4ac4553b8607de8c0d4196bb9f5
Victor.G <victorgranda1992@gmail.com>
2023-01-17 23:40:52 +0100

    Add console.log function calls to show the results in a user-friendly(ish) way

--- js/index.js
+++ js/index.js
@@ -1,9 +1,1 @@
 import strictEquals from "./strictEquals.js";
-strictEquals(1, 1);
-strictEquals(NaN, NaN);
-strictEquals(0, -0);
-strictEquals(-0, 0);
-strictEquals(1, "1");
-strictEquals(true, false);
-strictEquals(false, false);
-strictEquals("Water", "oil");
--- js/strictEquals.js
+++ js/strictEquals.js
@@ -1,16 +1,23 @@
 const strictEquals = (valueA, valueB) => {
   if (isNaN(valueA) && isNaN(valueB)) {
-    return console.log(`${valueA} and ${valueB} -> ${false}`);
+    return `${valueA} and ${valueB} -> ${false}`;
   }
   if (Object.is(valueA, -0) && Object.is(valueB, 0)) {
-    return console.log(`${valueA} and ${valueB} -> ${true}`);
+    return `${valueA} and ${valueB} -> ${true}`;
   }
   if (Object.is(valueA, 0) && Object.is(valueB, -0)) {
-    return console.log(`${valueA} and ${valueB} -> ${true}`);
+    return `${valueA} and ${valueB} -> ${true}`;
   }
-  return console.log(
-    `${valueA} and ${valueB} -> ${Object.is(valueA, valueB)} `
-  );
+  return `${valueA} and ${valueB} -> ${Object.is(valueA, valueB)} `;
 };
+
+console.log(`${strictEquals(1, 1)}: should be true`);
+console.log(`${strictEquals(NaN, NaN)}: should be false`);
+console.log(`${strictEquals(0, -0)}: should be true`);
+console.log(`${strictEquals(-0, 0)}: should be true`);
+console.log(`${strictEquals(1, "1")}: should be false`);
+console.log(`${strictEquals(true, false)}: should be false`);
+console.log(`${strictEquals(false, false)}: should be true`);
+console.log(`${strictEquals("Water", "oil")}: should be false`);
 
 export default strictEquals;
